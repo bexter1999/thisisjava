cdeca6c304f743cd4a44ab096d0046cc44dae7d6
bexter1999 <bexter125@naver.com>
2022-08-22 12:43:52 +0900

    java

--- test01/src/test05/ShopService.java
+++
@@ -1,9 +0,0 @@
-package test05;
-
-public class ShopService {
-	private static ShopService singleton = new ShopService();
-	private ShopService(){}; /*½Ì±ÛÅæÀº private·Î »ý¼ºÀÚ¸¦ ¸¸µë*/
-	public static ShopService getInstance() {
-		return singleton;
-	}
-}
